336588602fd61659e2ef8f8c86c495b03b455fca
Владимир Смолко <@pt.local>
2016-07-20 14:26:48 +0300

    script for responsive catalog menu

---
+++ resp_menu.js
@@ -0,0 +1,10 @@
+﻿jQuery(document).ready(function($){	
+	if ($('#block-nice-menus-1 .content').children().length == 0){
+		jQuery('#block-nice-menus-2 .content').prepend('<input type="checkbox" id="showcat">');
+		jQuery('#block-nice-menus-2 .content').prepend('<label for="showcat">Catalog<label/>');
+	}
+	else{
+		jQuery('#block-nice-menus-1 .content').prepend('<input type="checkbox" id="showcat">');
+		jQuery('#block-nice-menus-1 .content').prepend('<label for="showcat">Каталог<label/>');
+	}	
+});
